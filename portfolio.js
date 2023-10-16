5de555ebb5d14c041f5a13842ca54a0d9884ce00
Sadiq-d-gent <138960197+Sadiq-d-gent@users.noreply.github.com>
2023-10-16 12:14:37 +0100

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,33 @@
+let MenuBtn = document.getElementById('MenuBtn')
+
+
+MenuBtn.addEventListener('click', function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-bars');
+})
+
+let typed = new Typed('.auto-input',{
+    strings: ['Web Developer!', 'Freelancer!', 'Full-stack Developer!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
+
+let navlinks = document.querySelectorAll('nav ul li a')
+
+let sections = document.querySelectorAll('section')
+
+window.addEventListener('scroll', function(){
+    const scrollPos = window.scrollY + 20
+    sections.forEach(section =>{
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navlinks.forEach(link => {
+                link.classList.remove('active');
+                if(section.getAttribute('id') === link.getAttribute('href').substring(1)){
+                    link.classList.add('active')
+                }
+            });
+        }
+    });
+})
